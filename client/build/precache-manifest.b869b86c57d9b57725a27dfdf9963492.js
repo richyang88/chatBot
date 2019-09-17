b56a2d8db49188e022f02451cf056392b3272a9b
Richard Yang <rich.yang88@gmail.com>
2019-09-17 09:50:05 -0400

    init commit

---
+++ client/build/precache-manifest.b869b86c57d9b57725a27dfdf9963492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05ff6fd6c6a6b19792d760a18160495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7826503cab32d0b0dd6",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "60d54c66eb6cf52bb511",
+    "url": "/static/js/2.ae6dacf0.chunk.js"
+  },
+  {
+    "revision": "c7826503cab32d0b0dd6",
+    "url": "/static/js/main.e16fd9d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
